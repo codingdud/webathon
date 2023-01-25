46a67a1aa3b6359096b11bede2ab96fa852f8271
darkhornet <animeshk384@gmail.com>
2023-01-25 16:40:46 +0530

    forgot passwd pade added

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ exports.register = (req, res)=>{
     const passcon = req.body.passcon; */
 
     const { user, mail, pass, passcon} = req.body;
-
+    //console.log(mail);
     db.query('SELECT email FROM temp WHERE email = ?',[mail], async (error, results) => {
         if(error){
             console.log(error);
@@ -42,8 +42,46 @@ exports.register = (req, res)=>{
                     message: "User registered"
                 });
             }
-        })
+        });
         //res.send("testing..");
     });
     //res.send("form submitted");
+}
+
+exports.login = (req,res)  =>{
+    console.log(req.body);
+    
+    /* const name = req.body.name;
+    const mail = req.body.mail;
+    const pass = req.body.pass;
+    const passcon = req.body.passcon; */
+
+    const {user, pass} = req.body;
+
+    db.query('SELECT pass FROM temp WHERE name = ?',[user], async (error, results) => {
+        if(error){
+            console.log(error);
+        }
+        if(results.length > 0){
+            return res.render('login',{
+                message: 'this name is already in use'
+            })
+        } else if (pass) {
+            return res.render('login',{
+                message: 'password is not same!'
+            });
+        }
+        let hashedPasswd = await bcrypt.hash(pass,2);
+        console.log(hashedPasswd);
+        db.query('INSERT INTO temp SET ?',{user:user,passwd:hashedPasswd,email:mail},(error,results)=>{
+            if(error){
+                console.log(error);
+            } else {
+                res.render('login',{
+                    message: "User registered"
+                });
+            }
+        });
+        //res.send("testing..");
+    });
 }
--- routes/pages.js
+++ routes/pages.js
@@ -11,5 +11,8 @@ router.get('/register',(req,res)=>{
 router.get('/login', function(req, res) {
     res.render('login')
 });
+router.get('/forgot', function(req, res) {
+    res.render('forgot')
+});
 
 module.exports = router;
